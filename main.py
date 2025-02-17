7c7c5be9d93cb7bf7265ce111e7c9302c640eeb5
Nguyễn Minh Tiến <57131836+tiennma2k23@users.noreply.github.com>
2025-02-17 15:15:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -137,4 +137,4 @@ def download_file(token):
         os.remove(file_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
